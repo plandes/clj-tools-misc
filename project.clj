20e7809bde8972233c9fe2970651bdacf0a83001
Paul Landes <landes@mailc.net>
2017-01-20 12:48:56 -0600

    removing unecessary deps; url

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
 (defproject com.zensols.tools/misc "0.1.0-SNAPSHOT"
   :description "Miscellaneous utilities"
-  :url "https://github.com/plandes/clj-nlp-annotation"
+  :url "https://github.com/plandes/clj-tools-misc"
   :license {:name "Apache License version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
-  :plugins [[lein-codox "0.9.5"]
+  :plugins [[lein-codox "0.10.1"]
             [org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
   :codox {:metadata {:doc/format :markdown}
           :project {:name "Miscellaneous utilities"}
           :output-path "target/doc/codox"}
   :source-paths ["src/clojure"]
   :javac-options ["-Xlint:unchecked"]
-  :exclusions [org.slf4j/slf4j-log4j12
-               ch.qos.logback/logback-classic]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; spreadsheet
                  [org.clojure/data.csv "0.1.2"]
